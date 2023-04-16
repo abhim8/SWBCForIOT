f9aee10b57ed16248bb341ed01af58cb530e56a8
abhim8 <abhilash.movva@gmail.com>
2023-04-16 18:59:19 +0530

    working v1

--- sourceCode.py
+++ sourceCode.py
@@ -10,9 +10,11 @@ import numpy as np
 import time
 from tkinter import messagebox
 import webbrowser
-
 from blockchain import *
 from block import *
+import base64
+import pickle
+import datetime
 
 global iot
 global labels
@@ -28,10 +30,60 @@ global propose
 global extension
 global running_time
 
+
+import hashlib
+import datetime
+
+class Block:
+    def __init__(self, index, timestamp, transactions, previous_hash):
+        self.index = index
+        self.timestamp = timestamp
+        self.transactions = transactions
+        self.previous_hash = previous_hash
+        self.nonce = 0
+        self.hash = self.calculate_hash()
+
+    def calculate_hash(self):
+        block_string = str(self.index) + str(self.timestamp) + str(self.transactions) + str(self.previous_hash) + str(self.nonce)
+        return hashlib.sha256(block_string.encode()).hexdigest()
+
+class Blockchain:
+    def __init__(self):
+        self.chain = []
+        self.transactions = []
+        self.peer = []
+        self.create_genesis_block()
+
+    def create_genesis_block(self):
+        genesis_block = Block(0, datetime.datetime.now(), "Genesis Block", "0")
+        self.chain.append(genesis_block)
+
+    def add_new_transaction(self, transaction):
+        self.transactions.append(transaction)
+
+    def mine(self):
+        last_block = self.chain[-1]
+        new_block = Block(last_block.index + 1, datetime.datetime.now(), self.transactions, last_block.hash)
+        self.chain.append(new_block)
+        self.transactions = []
+
+    def encrypt(self, data):
+      data_bytes = data.encode('utf-8')
+      encoded_data = base64.b64encode(data_bytes)
+      return encoded_data
+
+    def decrypt(self, data):
+        decoded_data = base64.urlsafe_b64decode(data).decode("utf-8")
+        return decoded_data
+
+    def addPeer(self, peer):
+        self.peer.append(peer)
+
+
 def calculateDistance(iot_x,iot_y,x1,y1):
   flag = False
   for i in range(len(iot_x)):
-    dist = math.sqrt((iot_x[i] - x1)*2 + (iot_y[i] - y1)*2)
+    dist = math.sqrt((iot_x[i] - x1)**2 + (iot_y[i] - y1)**2)
     if dist < 80:
       flag = True
       break
@@ -82,14 +134,14 @@ def SWBCThread(propose,running_time):
     global iot_list
     global window_list
     
-    def init (self, propose, running_time):
-      Thread.init (self)
+    def __init__(self, propose, running_time):
+      Thread.__init__(self)
       self.propose = propose
       self.running_time = running_time
     def run(self):
-      self.propose = 8
-      self.running_time = 8
-      blockchain = blockchain()
+      self.propose = 0
+      self.running_time = 0
+      blockchain = Blockchain()
       window_limit = int(window_list.get())
       num_transfer = int(iot_list.get())
       index = 0
@@ -104,13 +156,13 @@ def SWBCThread(propose,running_time):
           x = iot_x[iotID]
           y = iot_y[iotID]
           canvas.delete(labels[iotID])
-          lbl = canvas.create_text(x+20,y-18,fill="red",font="Times 10 italic bold", text="I0T "+str(iotID))
+          lbl = canvas.create_text(x+20,y-10,fill="red",font="Times 10 italic bold", text="I0T "+str(iotID))
           labels[iotID] = lbl
           count = 'IoT'+str(iotID)+","+str(random.randint(25,45))
           text.insert (END, "Generated Data : "+count+"\n")
           enc = blockchain.encrypt(count)
-          enc = str(base64.b64decode(enc), "utf-8")
-          text.insert(END,"AES encrypted data : "+enc+". Mining pending will done after 18 blocks\n")
+          enc = str(base64.b64encode(enc), 'utf-8')
+          text.insert(END,"AES encrypted data : "+enc+". Mining pending will done after 10 blocks\n")
           blockchain.addPeer(enc)
           self.propose = self.propose + 1
           if len(blockchain.peer) >= 10:
@@ -128,39 +180,39 @@ def SWBCThread(propose,running_time):
               finishtime = time.time()
               self.running_time = self.running_time + (finishtime - starttime)
           blockchain.peer.clear()
-        text.insert (END, 'Mining done and saved recent blocks to BC DB.txt file\n')
-        time.sleep(1)
-        canvas.delete(labels[iotID])
-        lbl = canvas.create_text(x+20,y-18,fill="darkblue",font="Times 10 italic bold", text="I0T "+str(iotID))
-        labels[iotID] = lbl
-        output = '<html><body><center><br/><br/><table align=center border=1><tr><th>Encrypted Packet</th><th>Decrypted Packet</th>'
-        output+='<th>Previous Hash</th><th>Packet Index</th><th>Current Hash</th><th>Timestamp</th></tr>'
-        for i in range(len(blockchain.chain)):
-          if i>0:
-            b = blockchain.chain[i]
-            data = b.transactions[0]
-            data = base64.b64decode(data)
-            decrypt = blockchain.decrypt(data)
-            text.insert(END,str(data)+" "+str(decrypt.decode("utf-8"))+" "+str(b.previous_hash)+" "+str(b.index)+" "+str(b.hash)+" "+str(datetime.fromtimestamp(b.timestamp)))
-            print(str(data)+" "+str(decrypt.decode("utf-8"))+" "+str(b.previous_hash)+" "+str(b.index)+" "+str(b.hash)+" "+str(datetime.fromtimestamp(b.timestamp)))
-            output+='<tr><td>'+str(data)+'</td><td>'+str(decrypt.decode("utf-8"))+'</td><td>'+str(b.previous_hash)+'</td>'
-            output+='<td>'+str(b.index)+'</td><td>'+str(b.hash)+'</td><td>'+str(datetime.fromtimestamp(b.timestamp))+'</td></tr>'
-            canvas.delete(labels[0])
-            x = iot_x[0]
-            y = iot_y[0]
-            lbl = canvas.create_text(x+28,y-18,fill="red",font="Times 7 italic bold", text="T0T Cloud")
-            labels[0] = lbl
-            time.sleep(2)
-            lbl = canvas.create_text(x+20,y-10,fill="darkblue",font="Times 7 italic bold", text="10T Cloud")
-            labels[0] = lbl
-            messagebox.showinfo("Propose Sliding Window Blockchain Simulation Completed", "Total transfer packets are "+str(self.propose)+"\nTotal execution time : "+str(round(self.running_time,5))+" M.S for window size "+str(window_limit))
-            f = open("table.html", "w")
-            f.write(output)
-            f.close()
-            f = open("propose.txt", "w")
-            f.urite(str(self.propose))
-            f.close()
-            webbrowser. open("table.html",new=2)
+          text.insert (END, 'Mining done and saved recent blocks to BC DB.txt file\n')
+          time.sleep(1)
+          canvas.delete(labels[iotID])
+          lbl = canvas.create_text(x+20,y-10,fill="darkblue",font="Times 10 italic bold", text="I0T "+str(iotID))
+          labels[iotID] = lbl
+          output = '<html><body><center><br/><br/><table align=center border=1><tr><th>Encrypted Packet</th><th>Decrypted Packet</th>'
+          output+='<th>Previous Hash</th><th>Packet Index</th><th>Current Hash</th><th>Timestamp</th></tr>'
+          for i in range(len(blockchain.chain)):
+            if i>0:
+              b = blockchain.chain[i]
+              data = b.transactions[0]
+              data = base64.b64decode(data)
+              decrypt = blockchain.decrypt(data)
+              text.insert(END,str(data)+" "+str(decrypt)+" "+str(b.previous_hash)+" "+str(b.index)+" "+str(b.hash)+" "+str(datetime.datetime.fromtimestamp(b.timestamp)))
+              print(str(data)+" "+str(decrypt)+" "+str(b.previous_hash)+" "+str(b.index)+" "+str(b.hash)+" "+str(datetime.datetime.fromtimestamp(b.timestamp)))
+              output+='<tr><td>'+str(data)+'</td><td>'+str(decrypt)+'</td><td>'+str(b.previous_hash)+'</td>'
+              output+='<td>'+str(b.index)+'</td><td>'+str(b.hash)+'</td><td>'+str(datetime.datetime.fromtimestamp(b.timestamp))+'</td></tr>'
+          canvas.delete(labels[0])
+          x = iot_x[0]
+          y = iot_y[0]
+          lbl = canvas.create_text(x+20,y-10,fill="red",font="Times 7 italic bold", text="I0T Cloud")
+          labels[0] = lbl
+          time.sleep(2)
+          lbl = canvas.create_text(x+20,y-10,fill="darkblue",font="Times 7 italic bold", text="I0T Cloud")
+          labels[0] = lbl
+          # messagebox.showinfo("Propose Sliding Window Blockchain Simulation Completed", "Total transfer packets are "+str(self.propose)+"\nTotal execution time : "+str(round(self.running_time,5))+" M.S for window size "+str(window_limit))
+          f = open("table.html", "w")
+          f.write(output)
+          f.close()
+          f = open("propose.txt", "w")
+          f.write(str(self.propose))
+          f.close()
+          webbrowser.open("table.html",new=2)
   newthread = RunThread(propose, running_time)
   newthread.start()
 
@@ -185,83 +237,84 @@ def SWBCExtensionThread(extension):
     global window_list
 
 
-def _init_(self,extension):
-  Thread._init_(self)
-  self.extension = extension
+    def __init__(self,extension):
+      Thread.__init__(self)
+      self.extension = extension
 
-def run(self):
-  extension_time = 0
-  self.extension = 0
-  datalist = []
-  blockchain = Blockchain()
-  window_limit = int(window_list.get())
-  num_transfer = int(iot_list.get())
-  index = 0
-  for i in range(0,num_transfer):
-    iotID = random.randint(1,19)
-    value = random.randint(25,45) #generating IOT data randomly
-    if value not in datalist:
-      datalist.append (value)
-      x = lot_x[iotID]
-      y = iot_y[iotID]
-      canvas.delete(labels[iotID])
-      lbl = canvas.create_text(x+20,y-18,fill="red",font="Times 18 italic bold", text="I0T "+str(iotID))
-      labels[iotID] = lbl
-      count = 'IoT'+str(iotID)+","+str(value)
-      text.insert (END, "Generated Data : "+count+"\n")
-      enc = blockchain.encrypt(count) #encryting data
-      enc = str(basebd.bbdencode(enc), 'utf-8')
-      text.insert (END, "AES encrypted data : "+enc+". Mining pending will done after 10 blocks\n")
-      blockchain.addPeer(enc) #adding data to block chain
-      self.extension = self.extension + 1
-      if len(blockchain.peer) >= 10:
-        for k in range(len(blockchain.peer)):
-          if len(blockchain.chain) == window_limit: #checking sliding window size and if exceed 
-            blck = blockchain.chain.pop(0) #then pop or remove first old block
-            name = time.strftime("%d-%m-%Y-%H-%M-%S") + "txt"
-            with open('remove/remove_'+str(index)+' '+name, 'wb') as output:
-              pickle.dump(blck, output, pickle.HIGHEST_PROTOCOL)
-            index = index + 1
-          text.insert(END, "Window size exceed & saved old block to remove folder and maintain recent blocks\n")
-          starttime = time.time()
-          blockchain.add_new_transaction(blockchain.peer[k])
-          blockchain.mine()
-          finishtime = time.time()
-          extension_time = extension_time + (finishtime - starttime)
-          #print(extension_time)
-          blockchain.peer.clear()
-          text.insert(END, 'Mining done and saved recent blocks to BC_DB.txt file\n')
-          time.sleep(1)
+    def run(self):
+      extension_time = 0
+      self.extension = 0
+      datalist = []
+      blockchain = Blockchain()
+      window_limit = int(window_list.get())
+      num_transfer = int(iot_list.get())
+      index = 0
+      for i in range(0,num_transfer):
+        iotID = random.randint(1,19)
+        value = random.randint(25,45) #generating IOT data randomly
+        if value not in datalist:
+          datalist.append(value)
+          x = iot_x[iotID]
+          y = iot_y[iotID]
           canvas.delete(labels[iotID])
-          lbl = canvas.create_text(x+28,y-18,fill="darkblue",font="Times 10 italic bold", text="I0T "+str(iotID))
+          lbl = canvas.create_text(x+20,y-18,fill="red",font="Times 10 italic bold", text="I0T "+str(iotID))
           labels[iotID] = lbl
-          for i in range(len(blockchain.chain)):
-            if i>0:
-              b = blockchain.chain[i]
-              data = b.transactions[0]
-              data = basef4.bbddecode(data)
-              decrypt = blockchain.decrypt(data)
-              text.insert(END,str(data)+" "+str(decrypt.decode("utf-8"))+" "+str(b.previous_hash)+" "+str(b.index)+" "+str(b.hash)+" "+str(datetime.fromtimestamp(b.timestamp)))
-              print(str(data)+" "+str(decrypt.decode("utf-8"))+" "+str(b.previous_hash)+" "+str(b.index)+" "+str(b.hash)+" "+str(datetime.fromtimestamp(b.timestamp)))
-              canvas.delete(labels[0])
-              x = iot_x[0]
-              y = iot_y[0]
-              lbl = canvas.create_text(x+20,y-10,fill="red",font="Times 7 italic bold", text="I0T Cloud")
-              labels[0] = lbl
-              time.sleep(2)
-              lbl = canvas.create_text(x+20,y-10,fill="darkblue",font="Times 7 italic bold", text="I0T Cloud")
-              labels[0] = lbl
-              messagebox.showinfo( "Extension without duplicate packets Sliding Window Blockchain Simulation Completed","Total transfer packets are "+str(self.extension)+"\nTotal extension execution time : "+str(round(extension_time,5))+" M.S for window size "+str(window_limit))
-              f = open("extension.txt", "w")
-              f.write(str(self.extension))
-              f.close()
+          count = 'IoT'+str(iotID)+","+str(value)
+          text.insert(END, "Generated Data : "+count+"\n")
+          enc = blockchain.encrypt(count) #encryting data
+          enc = str(base64.b64decode(enc), 'utf-8')
+          text.insert(END, "AES encrypted data : "+enc+". Mining pending will done after 10 blocks\n")
+          blockchain.addPeer(enc) #adding data to block chain
+          self.extension = self.extension + 1
+          if len(blockchain.peer) >= 10:
+            for k in range(len(blockchain.peer)):
+              if len(blockchain.chain) == window_limit: #checking sliding window size and if exceed 
+                blck = blockchain.chain.pop(0) #then pop or remove first old block
+                name = time.strftime("%d-%m-%Y-%H-%M-%S") + "txt"
+                with open('remove/remove_'+str(index)+' '+name, 'wb') as output:
+                  pickle.dump(blck, output, pickle.HIGHEST_PROTOCOL)
+                index = index + 1
+              text.insert(END, "Window size exceed & saved old block to remove folder and maintain recent blocks\n")
+              starttime = time.time()
+              blockchain.add_new_transaction(blockchain.peer[k])
+              blockchain.mine()
+              finishtime = time.time()
+              extension_time = extension_time + (finishtime - starttime)
+              print(extension_time)
+              blockchain.peer.clear()
+              text.insert(END, 'Mining done and saved recent blocks to BC_DB.txt file\n')
+              time.sleep(1)
+              canvas.delete(labels[iotID])
+              lbl = canvas.create_text(x+20,y-10,fill="darkblue",font="Times 10 italic bold", text="I0T "+str(iotID))
+              labels[iotID] = lbl
+              for i in range(len(blockchain.chain)):
+                if i>0:
+                  b = blockchain.chain[i]
+                  data = b.transactions[0]
+                  # data = base64.b64decode(data)
+                  decrypt = blockchain.decrypt(data)
+                  print(data)
+                  text.insert(END,str(data)+" "+str(decrypt)+" "+str(b.previous_hash)+" "+str(b.index)+" "+str(b.hash)+" "+str(datetime.datetime.fromtimestamp(b.timestamp)))
+                  print(str(data)+" "+str(decrypt)+" "+str(b.previous_hash)+" "+str(b.index)+" "+str(b.hash)+" "+str(datetime.datetime.fromtimestamp(b.timestamp)))
+                  canvas.delete(labels[0])
+                  x = iot_x[0]
+                  y = iot_y[0]
+                  lbl = canvas.create_text(x+20,y-10,fill="red",font="Times 7 italic bold", text="I0T Cloud")
+                  labels[0] = lbl
+                  time.sleep(1)
+                  lbl = canvas.create_text(x+20,y-10,fill="darkblue",font="Times 7 italic bold", text="I0T Cloud")
+                  labels[0] = lbl
+                  messagebox.showinfo( "Extension without duplicate packets Sliding Window Blockchain Simulation Completed","Total transfer packets are "+str(self.extension)+"\nTotal extension execution time : "+str(round(extension_time,5))+" M.S for window size "+str(window_limit))
+                  f = open("extension.txt", "w")
+                  f.write(str(self.extension))
+                  f.close()
   newthread = ExtensionThread(extension)
   newthread.start()
 
 
 def extension():
   global extension
-  extension = 0
+  extension = 0 # type: ignore
   text.delete('1.0', END)
   SWBCExtensionThread(extension)
 
@@ -285,7 +338,7 @@ def graph():
   y_pos = np.arange(len(bars))
   plt.bar(y_pos, height)
   plt.xticks(y_pos, bars)
-  plt.shou()
+  plt.show()
 
 def Main():
   global window_list
@@ -293,10 +346,10 @@ def Main():
   global canvas
   global iot_list
   root = tkinter.Tk()
-  root.geometry ("1308x1208")
+  root.geometry ("1300x1200")
   root.title("Sliding Window Blockchain Architecture for Internet of Things")
   root.resizable(True, True)
-  fontl = ('times', 12, 'bold')
+  font1 = ('times', 12, 'bold')
   
   canvas = Canvas(root, width = 800, height = 700)
   canvas.pack()
@@ -315,7 +368,7 @@ def Main():
   iot_list.place(x=970,y=10)
   iot_list.current(0)
   
-  iot_list.config(font=fontl)
+  iot_list.config(font=font1)
   
   for i in range(15,30):
     mid.append(str(i))
@@ -326,7 +379,7 @@ def Main():
   iot_list.config(font=font1)
   l2 = Label(root, text='Window Size:')
   l2.config(font=font1)
-  l2.place(x=828,y=68)
+  l2.place(x=820,y=60)
   
   window = []
   window.append(5)
@@ -341,9 +394,9 @@ def Main():
   createButton.place(x=820,y=110)
   createButton.config(font=font1)
   
-  swhcButton = Button(root, text="Run SWBC Simulation", command=runSWBC)
+  swbcButton = Button(root, text="Run SWBC Simulation", command=runSWBC)
   swbcButton.place(x=820,y=160)
-  swhcButton.config(font=font1)
+  swbcButton.config(font=font1)
   
   extensionButton = Button(root, text="Run SWBC Simulation Extension Algorithm", command=extension)
   extensionButton.place(x=820,y=210)
@@ -361,5 +414,5 @@ def Main():
   
   root.mainloop()
 
-if __name__== 'main_':
+if __name__== '__main__':
   Main ()
